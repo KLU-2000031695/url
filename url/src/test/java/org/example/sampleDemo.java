50a28c31a3f72336a62e2e2ec6ba42ca273f7e30
KLU-2000031695 <96164974+KLU-2000031695@users.noreply.github.com>
2023-02-16 08:04:32 +0530

    Add files via upload

---
+++ url/src/test/java/org/example/sampleDemo.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+    public class sampleDemo {
+        @Test
+        public void test(){
+            System.out.println("welcome");
+            WebDriver driver;
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.get("https://gmail.com");
+
+        }
+
+    }
+
